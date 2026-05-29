f2b32ebae2632ef38ba7d7f0a53cb488709031b6
SebiA <seb.alderucci@gmail.com>
2026-05-29 12:54:39 -0400

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import Navigator from "./Navigator";
+
+const Home = () => {
+    return (
+        <>
+        <Navigator></Navigator>
+        </>
+    );
+};
+
+export default Home;
---
+++ src/components/Navigator.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "@fontsource/raleway";
+
+const Navigator = () => {
+
+    return (
+        <>
+        <div className='nav-button'>
+            <button>About Me</button>
+            <button>Projects</button>
+            <button>Resume</button>
+            <button>Contact</button>
+        </div>
+    </>
+    );
+};
+
+export default Navigator;
