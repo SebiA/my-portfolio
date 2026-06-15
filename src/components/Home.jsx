d5879052b9d5e340735e756b1fa2320d95479746
SebiA <seb.alderucci@gmail.com>
2026-06-15 18:43:43 -0400

    added more things on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,28 @@ import Navigator from "./Navigator";
 import "@fontsource/merriweather-sans";
 
 const Home = () => {
-    return (
-        <>
-        <div className='name-label-top'>Sebastian Alderucci</div>
+  return (
+    <>
+      <div className="nav-bar-main">
+        <p className="name-label-top">Sebastian Alderucci</p>
         <Navigator></Navigator>
-        <h1 className='name-label-main'>Sebastian Alderucci</h1>
-        </>
-    );
+      </div>
+      <div className="home-content">
+        <div className="label-main">
+          <h1 className="name-label-main">Sebastian Alderucci</h1>
+          <h1 className="headline-label">Software Developer | Data Engineer</h1>
+          <p className='description-label'>
+            Data engineer at Travelers with a MEng. in Computer Science from the
+            University of Connecticut. Experience with building and managing
+            data pipelines and analyzing data from various sources. Continuous
+            learner with a passion for software development. Proactive and
+            detail oriented team player. Enthusiastic about using data
+            engineering techniques to help enhance software applications.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 import "@fontsource/raleway";
 
 const Navigator = () => {
-
-    return (
-        <>
-        <div className='nav-button'>
-            <button>Home</button>
-            <button>About Me</button>
-            <button>Projects</button>
-            <button>Resume</button>
-            <button>Contact</button>
+  return (
+    <>
+      <div className="nav-button-group">
+        <div className="nav-button">
+          <button>Home</button>
+          <button>About Me</button>
+          <button>Projects</button>
+          <button>Resume</button>
+          <button>Contact</button>
         </div>
+      </div>
     </>
-    );
+  );
 };
 
 export default Navigator;
