4da8f44593828b1086ad8642c90338f024b7b37e
SebiA <seb.alderucci@gmail.com>
2026-07-24 19:41:15 -0400

    updated description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,11 +14,11 @@ const Home = () => {
           <h1 className="headline-label">Software Developer | Data Engineer</h1>
           <p className='description-label'>
             Data engineer at Travelers with a MEng. in Computer Science from the
-            University of Connecticut. Experience with building and managing
+            University of Connecticut. Experience with building and maintaining
             data pipelines and analyzing data from various sources. Continuous
             learner with a passion for software development. Proactive and
-            detail oriented team player. Enthusiastic about using data
-            engineering techniques to help enhance software applications.
+            detail oriented team player. Enthusiastic about using software
+            engineering techniques to help enhance applications.
           </p>
         </div>
       </div>
