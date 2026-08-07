bbab78c76f9fc9b73dc0cd54b3e906346b4571c1
SebiA <seb.alderucci@gmail.com>
2026-08-07 15:00:57 -0400

    improve button design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,12 +4,12 @@ export default function NavBar({ setActiveTab }) {
   return (
     <>
       <div className="nav-button-group">
-        <div className="nav-button">
-          <button onClick={() => setActiveTab("home")}>Home</button>
-          <button onClick={() => setActiveTab("about")}>About</button>
-          <button onClick={() => setActiveTab("projects")}>Projects</button>
-          <button onClick={() => setActiveTab("resume")}>Resume</button>
-          <button onClick={() => setActiveTab("contact")}>Contact</button>
+        <div class="nav-button">
+          <button type="button" onClick={() => setActiveTab("home")}>Home</button>
+          <button type="button" onClick={() => setActiveTab("about")}>About</button>
+          <button type="button" onClick={() => setActiveTab("projects")}>Projects</button>
+          <button type="button" onClick={() => setActiveTab("resume")}>Resume</button>
+          <button type="button" onClick={() => setActiveTab("contact")}>Contact</button>
         </div>
       </div>
     </>
